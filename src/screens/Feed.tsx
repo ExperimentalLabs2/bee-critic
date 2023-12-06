09955cb00d6fd34494c0860ffe324649f447c6cd
Enzo Maitan <maitanfavaroenzo@gmail.com>
2023-12-05 19:54:06 -0500

    Adds header hiding when scrolling down

--- src/screens/Feed.tsx
+++ src/screens/Feed.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, SafeAreaView, FlatList } from 'react-native'
+import { ScrollView } from 'react-native-virtualized-view'
 
 import Header from '../components/Header';
 import Card from '../components/Card';
@@ -8,14 +9,15 @@ import mockCard from '../../mockdata/mockCard';
 import { StatusBar } from 'expo-status-bar';
 
 export default function Feed() {
-
   return (
-    <SafeAreaView className="flex-1 bg-white h-full" >
+    <SafeAreaView className="flex-1">
+      <ScrollView>
         <Header />
-          <View className="mt-3 flex-1 justify-center items-center">
-            <FlatList data={mockCard} renderItem={Card} />
-          </View>
+        <View className="mt-3 justify-center items-center bg-purple-500">
+              <FlatList data={mockCard} renderItem={Card}/>
+        </View>
         <StatusBar style="auto" />
+      </ScrollView>
     </SafeAreaView>
   )
 }
